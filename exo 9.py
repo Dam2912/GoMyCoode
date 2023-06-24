c9360e26ae4755da2c396961cab92394c8a78480
Dam2912 <135437956+Dam2912@users.noreply.github.com>
2023-06-24 14:16:38 +0100

    Delete exo 9.py

--- exo 9.py
+++
@@ -1,8 +0,0 @@
-# Demande à l'utilisateur d'entrer plusieurs éléments séparés par des espaces
-elements = input("Entrez plusieurs éléments séparés par des espaces : ")
-
-# Divise la chaîne de caractères en une liste d'éléments
-liste_elements = elements.split()
-
-# Affiche la liste d'éléments
-print("Liste d'éléments :", liste_elements)
